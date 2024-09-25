ded321d2818b55d035e281ea606230c7f65b673c
Cherly23 <cherlyu664@gmail.com>
2024-09-25 18:55:06 +0700

    first commit

---
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/Rifa1/src/rifa1/Rifa1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rifa1;
+
+/**
+ *
+ * @author cherly
+ */
+public class Rifa1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long c = 4100;
+        int i = 4;
+        double a = 4.5;
+        boolean b = true;
+        String nama = "Cherly Ma,rifatul U.";
+        char x ='X';
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(nama);
+        System.out.println(x);        
+    }
+    
+}
