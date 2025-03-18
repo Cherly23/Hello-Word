e1d82398e36e176bf89ec624027a11b3ae369e38
Cherly <cherlyu664@gmail.com>
2025-03-18 17:52:15 +0700

    add Hello Word

--- Users/cherl/OneDrive/Documents/NetBeansProjects/T2 HelloWord/src/t2/helloword/T2HelloWord.java
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/T2 HelloWord/src/t2/helloword/T2HelloWord.java
@@ -3,18 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package t2.helloword;
-
-/**
- *
- * @author cherly
- */
 public class T2HelloWord {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Word");
     }
     
 }
