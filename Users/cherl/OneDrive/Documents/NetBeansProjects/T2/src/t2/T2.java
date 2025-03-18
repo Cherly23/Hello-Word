8a1885ea6346614493bd1772008d07646595193b
Cherly <cherlyu664@gmail.com>
2025-03-18 17:37:23 +0700

    add Hello Word

--- Users/cherl/OneDrive/Documents/NetBeansProjects/T2/src/t2/T2.java
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/T2/src/t2/T2.java
@@ -10,12 +10,8 @@ package t2;
  */
 public class T2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        System.out.println("Hello Word");        
     }
     
 }
