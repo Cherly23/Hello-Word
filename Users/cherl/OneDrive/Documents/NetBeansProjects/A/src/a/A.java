4c2a1c309a887f84fe403f2066e014ff27ff139c
Cherly <cherlyu664@gmail.com>
2025-03-19 12:56:47 +0700

    Fr 2

--- Users/cherl/OneDrive/Documents/NetBeansProjects/A/src/a/A.java
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/A/src/a/A.java
@@ -15,7 +15,7 @@ public class A {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hai");
+        System.out.println("Hai My Name is Cherly");
     }
     
 }
