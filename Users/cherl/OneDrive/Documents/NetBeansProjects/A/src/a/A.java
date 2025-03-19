86f78273285ea07bbc6500a272f4d25262ed25d6
Cherly <cherlyu664@gmail.com>
2025-03-19 12:58:29 +0700

    Fr 3

--- Users/cherl/OneDrive/Documents/NetBeansProjects/A/src/a/A.java
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/A/src/a/A.java
@@ -16,6 +16,7 @@ public class A {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hai My Name is Cherly");
+        System.out.println("My Hobby : Cooking");
     }
     
 }
