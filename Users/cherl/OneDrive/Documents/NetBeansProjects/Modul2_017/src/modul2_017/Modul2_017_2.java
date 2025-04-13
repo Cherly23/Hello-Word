f96003776259a7e91e9455c345f52d03c7e35c73
Cherly <cherlyu664@gmail.com>
2025-04-13 22:54:20 +0700

    T4 Modul3

--- Users/cherl/OneDrive/Documents/NetBeansProjects/Modul2_017/src/modul2_017/Modul2_017_2.java
+++ Users/cherl/OneDrive/Documents/NetBeansProjects/Modul2_017/src/modul2_017/Modul2_017_2.java
@@ -4,41 +4,41 @@
  */
 package modul2_017;
 
-import java.util.Scanner; // Mengimport Scanner  untuk imput dari pengguna
+import java.util.Scanner; 
 
 public class Modul2_017_2 {
     public double luas_lingkaran(int diameter) {
-        int jari_jari = diameter / 2; // Mengambil jari-jari dari diametter
-        double luas = Math.PI * Math.pow(jari_jari, 2); // Lumus luas lingkaran
-        return luas; // Mengambilkan hasil luas
+        int jari_jari = diameter / 2;
+        double luas = Math.PI * Math.pow(jari_jari, 2); 
+        return luas; 
     }
     public double volume_tabung(int diameter, double tinggi) {
         int jari_jari = diameter / 2;
-        double total = Math.PI * Math.pow(jari_jari, 2) * tinggi; // Rumus volume tabung
+        double total = Math.PI * Math.pow(jari_jari, 2) * tinggi; 
         return total;
     }
     public double volume_kerucut(int diameter, double tinggi){
         int jari_jari = diameter /2;
-        double total = (Math.PI * Math.pow(jari_jari, 2) * tinggi) * tinggi / 3;
+        double total = (Math.PI * Math.pow(jari_jari, 2) * tinggi) / 3;
         return total;
     }
     public double volume_bola (int diameter) {
         int jari_jari = diameter / 2;
-        double total = (4 * Math.PI * Math.pow(jari_jari, 3)) / 3; // Rumus volume bola 
+        double total = (4 * Math.PI * Math.pow(jari_jari, 3)) / 3; 
         return total;
     }
     public static void main(String[] args) {
-        Modul2_017_2 count = new Modul2_017_2(); // membuat objek dari class 
-        Scanner input = new Scanner(System.in); // Scanner untuk membaca input dari user
+        Modul2_017_2 count = new Modul2_017_2(); 
+        Scanner input = new Scanner(System.in);
         try {
-            String status = ""; double hasil = 0; // Variabel untuk menampung status dan hasil
+            String status = ""; double hasil = 0; 
             System.out.println("Pilihan : \n"
                     + "1. Luas Lingkaran\n"
                     + "2. Volume Tabung\n"
                     + "3. Volume Kerucut\n"
                     + "4. Vollulme Bola\n");
         System.out.print("Masukkan Pilihan : ");
-        byte pilihan = input.nextByte(); // Menyimpan pilihan user (1-4)
+        byte pilihan = input.nextByte();
         
         if (pilihan == 1) {
             System.out.print("Masukkan Diameter : ");
@@ -62,14 +62,15 @@ public class Modul2_017_2 {
         } else if (pilihan == 4) {
             System.out.print ("Masukkan Diameter : ");
             int diameter = input.nextInt();
+            hasil = count.volume_bola(diameter);
             status = "Menghitung Volume Bola";
         } else {
-            status = "Pilihan tidak valid, harus antara 1-4"; // Jika pilihannya tidak ada
+            status = "Pilihan tidak valid, harus antara 1-4"; 
         }
          System.out.println("\nStatus : " + status);
          System.out.println("Hasil  : " + hasil);   
         } catch (Exception e) {
-            System.out.println("Inputan Tidak Valid");
+            System.out.println("Input Tidak Valid, Silahkan Coba Lagi");
         }
     }
 }
